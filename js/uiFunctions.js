ffc740fc4c107f45bd4cbefc5257cad8804e6f20
kylehovey <sudokyle@gmail.com>
2016-04-17 00:36:55 -0600

    First Website

---
+++ js/uiFunctions.js
@@ -0,0 +1,12 @@
+//CALSim UI Functions
+//Author: Kyle Hovey
+
+function showHelp(){
+	swal({
+		title: "Editing Commands:",
+		text: rules,
+		html: true
+	}, function(){
+		helpDisplayed = false;
+	});
+}
