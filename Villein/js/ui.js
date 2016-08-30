f42b52ea010215942323df733693737a1b5bd277
kylehovey <khoveyc@gmail.com>
2016-08-29 21:54:38 -0600

    Version 1

--- Villein/js/ui.js
+++ Villein/js/ui.js
@@ -2,9 +2,23 @@
 // Author: Kyle Hovey
 
 $(function(){
-		//Define the utility script object
+		// Define the utility script object
 		_ = new numUtils();
 
-		//Make the main digit
+		// Make the main digit
 		mainDigit = new Villein("#output", 0);
+
+		// Add listeners
+		$("#submit").on("click", function(event){
+				// Find user number
+				var num = +$("#number-entry").val();
+
+				// Set the digit
+				mainDigit.setDigit(num);
+		});
+
+		$("#clear").on("click", function(event){
+				// Clear the input
+				$("#number-entry").val("");
+		});
 });
