0c84910b0f76a3845b4806a5a613187e4aedbc61
kylehovey <sudokyle@gmail.com>
2016-08-29 20:42:35 -0600

    Added Villein digit class visual.

---
+++ Villein/js/util.js
@@ -0,0 +1,49 @@
+// Number utilities
+
+function numUtils(){
+		this.toFour = function(num){
+				// Create the output number
+				var out = 0;
+
+				// Temporary variables
+				var digit = 0,
+				placeValue = 0;
+
+				// For a Villein digit, num < 4^5 = 1024
+				for (var i = 4; i >= 0; i--){
+						// Find value at place i
+						placeValue = 4**i;
+
+						// If number contains value at digit i
+						if (num - placeValue >= 0){
+								// Find digit
+								digit = (num - num % placeValue)/placeValue;
+
+								// Shift it into out
+								out += (10**i)*digit;
+
+								// Remove what we just represented
+								num %= placeValue;
+						}
+				}
+
+				// Return the final number
+				return out;
+		};
+
+		// Basic number structures
+		this.structures = [
+				[[-1, 0]],
+				[[0, 1], [1, 0]],
+				[[0, 1], [0, 1], [1, 0]]
+		];
+
+		// Digit locations
+		this.digitLocations = [
+				[2, 2],
+				[1, 1],
+				[3, 1],
+				[3, 3],
+				[1, 3]
+		];
+};
--- Villein/js/villein.js
+++ Villein/js/villein.js
@@ -3,14 +3,53 @@
 // Villein arithmetic is in base 4, and each grid
 // 5 base-4 digits (representing 0-1023 in decimal)
 
-function Villein(){
-		this.toFour = function(num){
-				//Create the output number
-				var out = 0;
+function Villein(id, n){
+		// Create the canvas
+		this.canvas = $(id).append('<canvas width=200 height=200 class="villein-digit"></canvas>').children(".villein-digit")[0];
 
-				// For a Villein digit, num < 4^5 = 1024
-				while (num > 0){
+		// Create context
+		this.context = this.canvas.getContext("2d");
 
+		// Create the internal digit structure
+		this.connections = [];
+
+		// Sets digit
+		this.setDigit = function(num){
+				// Find number in base four
+				num = _.toFour(num);
+
+				// Clear the canvas
+				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+				// Clear the grid
+				this.grid = [[], [], [], [], []];
+
+				// Determine the spaces inbetween dots
+				var xStep = this.canvas.width/4;
+				var yStep = this.canvas.height/4;
+
+				// Draw the circles and record their locations
+				for (var x = 0; x < 5; x++){
+						for (var y = 0; y < 5; y++){
+								// Memorize the location
+								this.grid[x].push({
+										x : x*xStep,
+										y : y*yStep
+								});
+
+								// Draw the circle
+								this.context.beginPath();
+								this.context.arc(x*xStep, y*yStep, 15, 0, Math.PI*2);
+								this.context.fillStyle = "#4d91ff";
+								this.context.fill();
+								this.context.closePath();
+						}
 				}
-		};
+		}
+
+		// Set the digit to number provided
+		this.setDigit(n);
+				
+		// Return the object so that we can call it
+		return this;
 }
