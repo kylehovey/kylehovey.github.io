5f688490548dc3ecfce6878437b6d13363665604
kylehovey <khoveyc@gmail.com>
2016-09-07 13:51:18 -0600

    Finished version 1.

--- js/main.js
+++ js/main.js
@@ -10,29 +10,24 @@ $(function() {
 	wheel.markerPathFunction = markerPath().PieLineMarker;
 	wheel.markerEnable = true;
 
+	// Generate nav items
+	var navItems = new Array();
+	$.each(projects, function(i, project) {
+		navItems.push(project.nav);
+	});
+
 	// Add items
-	wheel.createWheel([
-			'CALSim',
-			'Villein\nTranslator',
-			'Euler\nGolf',
-			'Ulam\nSpinner'
-	]);
+	wheel.createWheel(navItems);
 
 	// Add listeners
-	// CALSim
-	wheel.navItems[0].navigateFunction = function() {
-		console.log("CALSim");
-	};
-	// Villein Translator
-	wheel.navItems[1].navigateFunction = function() {
-		console.log("Villein");
-	};
-	// Euler Golf
-	wheel.navItems[2].navigateFunction = function() {
-		console.log("Euler Golf");
-	};
-	// Ulam Spinner
-	wheel.navItems[3].navigateFunction = function() {
-		console.log("Ulam Spinner");
-	};
+	$.each(wheel.navItems, function(i, item) {
+		item.navigateFunction = function() {
+			$("#item-title").text(projects[i].title);
+			$("#item-description").text(projects[i].description);
+			$("#item-link").prop("href", projects[i].link);
+		};
+	});
+	
+	// Navigate to the first item
+	wheel.navItems[0].navigateFunction();
 });
--- js/projects.js
+++ js/projects.js
@@ -2,3 +2,29 @@
 //
 // Author: Kyle Hovey
 
+projects = [
+	{
+		title : "CALSim (2016)",
+		nav : "CALSim",
+		description : "This is a WireWorld Cellular Automata simulator written in JavaScript. WireWorld is a CA capable of logic simulation. In fact, it is Turing Complete! This in-browser interpreter allows you to create structures, copy, paste, delete, and pause/play/step simulations.",
+		link : "CALSim/index.html"
+	},
+	{
+		title : "Villein Number Translator (2016)",
+		nav : "Villein\nTranslator",
+		description : "In Cyan's new game Obduction there is an alien race called the Villeins. Part of the game is figuring out their numbering system, which uses a three-by-three grid on which five \"digits\" can lie. The whole system is base-4, and I had a bunch of fun figuring it out. I wanted to create a translator to quickly verify numbers, so I made one husing the html5 canvas.",
+		link : "Villein/index.html"
+	},
+	{
+		title : "Euler Golf (2015)",
+		nav : "Euler\nGolf",
+		description : "When I was attending Cuesta College in San Luis Obispo, I had an idea for a game over the Gaussian Integers (complex numbers of the form a+bi where a and b are both integers). I quickly found that the game was evil in difficulty, and as I shared it with friends I also learned that it was not just me. In light of this newfound challenge, I created an in-browser version as a Hackathon project. More info can be found on the site.",
+		link : "EulerGolf/index.html"
+	},
+	{
+		title : "Ulam Spinner (2016)",
+		nav : "Ulam\nSpinner",
+		description : "Some projects start with vague ideas and oversized ambition. Most of the time, those types of ideas crash and burn in a humble fire. Sometimes, though, they can result in something worthwhile. In this case I wanted to somehow use the fact that complex numbers encode modular arithmetic to express primality in a visual way. The Ulam Spiral is a famous mathematical object that writes integers spiraling out from the origin and colors primes differently than non-primes. What you get is countless surprising linear patterns. I wanted to do the same thing, but with \"complex\" spinners that represent spinning complex numbers that would align when the criteria for \"division\" was met. I still think it is possible, but in the mean time I have a configuration that uses colors and spinners in a pattern that is perhaps a bit too colorful.",
+		link : "UlamSpinner/index.html"
+	}
+]
