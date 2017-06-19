e942d63e52fbdb2c3b094032358af64e96ddc7c7
kylehovey <khoveyc@gmail.com>
2017-06-18 20:02:58 -0700

    Removed 8-Bit project until I can get another domain name.

--- js/projects.js
+++ js/projects.js
@@ -27,12 +27,12 @@ projects = [
 		description : "Some projects start with vague ideas and oversized ambition. Most of the time, those types of ideas crash and burn in a humble fire. Sometimes, though, they can result in something worthwhile. In this case I wanted to somehow use the fact that complex numbers encode modular arithmetic to express primality in a visual way. The Ulam Spiral is a famous mathematical object that writes integers spiraling out from the origin and colors primes differently than non-primes. What you get is countless surprising linear patterns. I wanted to do the same thing, but with \"complex\" spinners that represent spinning complex numbers that would align when the criteria for \"division\" was met. I still think it is possible, but in the mean time I have a configuration that uses colors and spinners in a pattern that is perhaps a bit too colorful.",
 		link : "UlamSpinner/index.html"
 	},
-	{
+  /*{
 		title : "8-Bit Computer (2012)",
 		nav : "8 Bit",
 		description : "One fateful night of playing Minecraft with my best friend and I was convinced that I had to learn more about the inner workings of a CPU. We had just installed a mod that added basic digital components to the game, and my friend was constructing a processor. I knew nothing about architecture at that point, but I wanted to learn more. I began my journey reading \"Digitial Computer Electronics\" by Malvino and started constructing the basic modular components of a computer. Eventually I had constructed a processor of my own design. The final product had EM interference which induced signalling issues, but the knowledge I gained benefits me to this day. I made a website and tutorial to aid anyone who wishes to acheive the same goal. I know of a few individuals so far that have used my instructions to build their own CPU.",
 		link : "http://8bitspaghetti.com"
-	},
+  },*/
 	{
 		title : "Fireplace (2016)",
 		nav : "Fireplace",
